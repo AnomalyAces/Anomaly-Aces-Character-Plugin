1800cfd58798f2cfc745748a214099209b11a67c
Anomaly Aces <anomalyaces@gmail.com>
2025-06-09 21:40:27 -0400

    local conversion

---
+++ addons/anomalyAcesCharacter/AceCharacterUtil/ace_move_stats.gd
@@ -0,0 +1,12 @@
+class_name AceMoveStats extends Object
+
+var displacement: Vector3 = Vector3.ZERO
+var direction: Vector3 = Vector3.ZERO
+var distance: float = 0.0
+var velocity: Vector3 = Vector3.ZERO
+
+func _init() -> void:
+	pass
+
+func get_max_speed() -> float:
+	return 0.0
